b031c8da00cc6cc103d631831ed0ad0b846462f4
Chet Corcos <ccorcos@gmail.com>
2018-02-18 19:06:47 -0800

    First

---
+++ declarations.ts
@@ -0,0 +1,10 @@
+declare module "*.mp3" {
+	const x: string
+	export = x
+}
+
+// declare module "*.mp" {
+// 	// import { AstNode } from "markdown-it-renderer/MarkdownParser"
+// 	const content: any
+// 	export = content
+// }
--- index.tsx
+++ index.tsx
@@ -1,24 +1,100 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { css } from "glamor"
+import * as song from "./song.mp3"
+import Component from "reactive-magic/component"
+import { Value } from "reactive-magic"
 
-css.global("html, body", {
-	margin: 0,
-	padding: 0,
-})
+const fftValue = new Value<Array<number>>(Array(88).fill(0))
+const context = new AudioContext()
+const processor = context.createScriptProcessor(4096, 1, 1)
 
-css.global("a", {
-	color: "inherit",
-	textDecoration: "none",
-})
+processor.onaudioprocess = function(event) {
+	const inputBuffer = event.inputBuffer
+	const outputBuffer = event.outputBuffer
+	// for (let channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
+	const channel = 0
+	const inputData = inputBuffer.getChannelData(channel)
+	const outputData = outputBuffer.getChannelData(channel)
+	for (let sample = 0; sample < inputBuffer.length; sample++) {
+		outputData[sample] = inputData[sample]
+	}
+	computeFFT(inputData)
+	// }
+}
 
-css.global("body", {
-	margin: "1em 2em",
-	maxWidth: "50em",
-	fontFamily: '-apple-system, "Helvetica", "Arial", sans-serif',
-	color: "#444",
-	tabSize: 4,
-})
+interface ComplexNumber {
+	real: number
+	imaginary: number
+}
 
-const root = document.getElementById("root")
-ReactDOM.render(<Draw />, root)
+function fourierSeries(buffer: Float32Array, freq: number) {
+	const sum = { real: 0, imaginary: 0 }
+	for (let i = 0; i < buffer.length; i++) {
+		// Fourier Series
+		// buffer[time] * Math.exp(-i * 2 * Math.PI * freq * time)
+
+		// Eulers Formula
+		// e^(i*x) = cos(x) + i * sin(x)
+
+		const time = i / context.sampleRate
+		const theta = -2 * Math.PI * freq * time
+
+		sum.real += buffer[i] + Math.cos(theta)
+		sum.imaginary += buffer[i] + Math.sin(theta)
+	}
+	return (
+		Math.sqrt(sum.real * sum.real + sum.imaginary * sum.imaginary) /
+		buffer.length
+	)
+}
+
+function midiToFreq(midi: number) {
+	return 27.5 * Math.pow(2, (midi - 21) / 12)
+}
+
+function computeFFT(buffer: Float32Array) {
+	const fft = fftValue.get()
+	for (let i = 0; i < 88; i++) {
+		// Piano notes range from 21 - 108
+		fft[i] = fourierSeries(buffer, midiToFreq(i + 21))
+	}
+	fftValue.set(fft)
+}
+
+class App extends Component {
+	private handleRef = (node: HTMLAudioElement | null) => {
+		if (node) {
+			const source = context.createMediaElementSource(node)
+			source.connect(processor)
+			processor.connect(context.destination)
+		}
+	}
+
+	view() {
+		return (
+			<div>
+				<audio ref={this.handleRef} src={song} controls={true} />
+				<div>
+					{fftValue.get().map((x, i) => {
+						return (
+							<div
+								key={i}
+								style={{
+									display: "inline-block",
+									background: `rgba(0,0,255,${x * 4})`,
+									border: "1px solid black",
+									height: 20,
+									width: 20,
+								}}
+							/>
+						)
+					})}
+				</div>
+			</div>
+		)
+	}
+}
+
+const div = document.getElementById("root") as HTMLDivElement
+ReactDOM.render(<App />, div)
