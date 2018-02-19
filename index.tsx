53bdd91039193cc40bf5ff1dab7f1a9e1f411640
Chet Corcos <ccorcos@gmail.com>
2018-02-18 19:57:02 -0800

    Piano

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { css } from "glamor"
-// Wave goes from midi notes 48-96
 import * as song from "./wave2.mp3"
 import Component from "reactive-magic/component"
 import { Value } from "reactive-magic"
@@ -92,18 +91,66 @@ class App extends Component {
 				<audio ref={this.handleRef} src={song} controls={true} />
 				<div>
 					{fftValue.get().map((x, i) => {
-						return (
-							<div
-								key={i}
-								style={{
-									display: "inline-block",
-									background: `rgba(0,0,255,${x * 4})`,
-									border: "1px solid black",
-									height: 20,
-									width: 20,
-								}}
-							/>
-						)
+						const width = 20
+						const height = 80
+						const style: React.CSSProperties = {
+							display: "inline-block",
+							border: "1px solid black",
+							boxSizing: "border-box",
+						}
+						const note = i % 12
+						switch (note) {
+							// White Notes
+							case 0:
+							case 2:
+							case 4:
+							case 5:
+							case 7:
+							case 9:
+							case 11: {
+								style.height = height
+								style.width = width
+							}
+						}
+
+						switch (note) {
+							// Black Notes
+							case 1:
+							case 3:
+							case 6:
+							case 8:
+							case 10: {
+								style.height = height * 0.6
+								style.width = width * 0.6
+								style.marginBottom = height * 0.4
+							}
+						}
+
+						switch (note) {
+							// Black notes to the right
+							case 0:
+							case 2:
+							case 5:
+							case 7:
+							case 9: {
+								style.marginRight = -width * 0.3
+							}
+						}
+
+						switch (note) {
+							// Black notes to the left
+							case 2:
+							case 4:
+							case 7:
+							case 9:
+							case 11: {
+								style.marginLeft = -width * 0.3
+							}
+						}
+
+						style.background = `rgba(0,0,255,${x * 20})`
+
+						return <div key={i} style={style} />
 					})}
 				</div>
 			</div>
