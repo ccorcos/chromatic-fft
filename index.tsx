4b555a4ca3cee41a904917c5ff85188902567aa0
Chet Corcos <ccorcos@gmail.com>
2018-02-18 19:43:41 -0800

    It works

--- index.tsx
+++ index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { css } from "glamor"
-import * as song from "./song.mp3"
+// Wave goes from midi notes 48-96
+import * as song from "./wave2.mp3"
 import Component from "reactive-magic/component"
 import { Value } from "reactive-magic"
+import * as _ from "lodash"
 
 const fftValue = new Value<Array<number>>(Array(88).fill(0))
 const context = new AudioContext()
@@ -12,15 +14,15 @@ const processor = context.createScriptProcessor(4096, 1, 1)
 processor.onaudioprocess = function(event) {
 	const inputBuffer = event.inputBuffer
 	const outputBuffer = event.outputBuffer
-	// for (let channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
 	const channel = 0
+
+	// data is -1 to 1
 	const inputData = inputBuffer.getChannelData(channel)
 	const outputData = outputBuffer.getChannelData(channel)
 	for (let sample = 0; sample < inputBuffer.length; sample++) {
 		outputData[sample] = inputData[sample]
 	}
-	computeFFT(inputData)
-	// }
+	computeFFT(Array.from(inputData))
 }
 
 interface ComplexNumber {
@@ -28,7 +30,17 @@ interface ComplexNumber {
 	imaginary: number
 }
 
-function fourierSeries(buffer: Float32Array, freq: number) {
+const a = 0.54
+const b = 1 - a
+function window(buffer: Array<number>) {
+	for (let i = 0; i < buffer.length; i++) {
+		// Hamming window
+		buffer[i] =
+			buffer[i] * (a - b * Math.cos(2 * Math.PI * i / (buffer.length - 1)))
+	}
+}
+
+function fourierSeries(buffer: Array<number>, freq: number) {
 	const sum = { real: 0, imaginary: 0 }
 	for (let i = 0; i < buffer.length; i++) {
 		// Fourier Series
@@ -40,8 +52,8 @@ function fourierSeries(buffer: Float32Array, freq: number) {
 		const time = i / context.sampleRate
 		const theta = -2 * Math.PI * freq * time
 
-		sum.real += buffer[i] + Math.cos(theta)
-		sum.imaginary += buffer[i] + Math.sin(theta)
+		sum.real += buffer[i] * Math.cos(theta)
+		sum.imaginary += buffer[i] * Math.sin(theta)
 	}
 	return (
 		Math.sqrt(sum.real * sum.real + sum.imaginary * sum.imaginary) /
@@ -53,12 +65,15 @@ function midiToFreq(midi: number) {
 	return 27.5 * Math.pow(2, (midi - 21) / 12)
 }
 
-function computeFFT(buffer: Float32Array) {
+function computeFFT(buffer: Array<number>) {
+	window(buffer)
+
 	const fft = fftValue.get()
 	for (let i = 0; i < 88; i++) {
 		// Piano notes range from 21 - 108
 		fft[i] = fourierSeries(buffer, midiToFreq(i + 21))
 	}
+
 	fftValue.set(fft)
 }
 
